94e4ab91c61233c07a8b964a495102ccab9373b2
Andrew Trigg <andrewt51@hotmail.com>
2015-10-13 20:18:19 -0700

    CRUD fully working and form control is operational.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,52 +16,60 @@ todoApp.controller('homeCtrl',function($scope,$http, readTasks){
     $scope.taskArray = [];
   }
   readTasks.retrieve()
-   .success(function(data, status) {
-      $scope.taskArray = data;
-    })
+  .success(function(data, status) {
+    $scope.taskArray = data;
+  })
   
   $scope.addTask =function (){
-    $scope.taskDetails.status = false;
-    $scope.taskArray.push($scope.taskDetails);
-    // console.log($scope.taskDetails);
+    $scope.reminder = false
+    console.log($scope.reminder);
+    if($scope.taskDetails && $scope.taskDetails.date && $scope.taskDetails.task){
+      $scope.taskDetails.status = false;
+      $scope.taskArray.push($scope.taskDetails);
+    
     $http.post('http://localhost:3000/addToList',
       $scope.taskDetails
-    ,function successCb(response){
-      // console.log(response)
+      ,function successCb(response){
+     
     }, function errorCb(response){
-      // console.log(response)
+      
     })
     $scope.taskDetails ={};
-  }
+  }else{
 
-  $scope.change = function (task){ 
-    task.status = !task.status;
-    console.log(task)
-    $http.put('http://localhost:3000/completeTask/'+task._id)
-    .then( function(data){
-      console.log(data);
-    })
-  }
-
-  $scope.removeTask =function(task){
-    $http.delete('http://localhost:3000/deleteTask/'+ task._id)
-    .then(function(data){
-      // console.log(data)
-    })
-    readTasks.retrieve()
-    .success(function(data, status) {
-      $scope.taskArray = data;
-    })
+  $scope.reminder =true;
+  console.log('helloe')
 
   }
+  
+}
+
+$scope.change = function (task){ 
+  task.status = !task.status;
+  console.log(task)
+  $http.put('http://localhost:3000/completeTask/'+task._id)
+  .then( function(data){
+    console.log(data);
+  })
+}
+
+$scope.removeTask =function(task){
+  $http.delete('http://localhost:3000/deleteTask/'+ task._id)
+  .then(function(data){
+    })
+  readTasks.retrieve()
+  .success(function(data, status) {
+    $scope.taskArray = data;
+  })
+
+}
 })
 
 todoApp.service('readTasks', function($http){
-   this.retrieve = function(){
-    return $http.get('http://localhost:3000/readList');
 
-  }
+ this.retrieve = function(){
+  return $http.get('http://localhost:3000/readList');
+}
 
 })
 
- 
